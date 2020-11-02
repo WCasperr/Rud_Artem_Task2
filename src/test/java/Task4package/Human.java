3f26f1b9c06036c53b988dc2d8bf1e0ceb0d916a
RudArtem <poseydon20001201@gmail.com>
2020-11-02 19:53:56 +0200

    AT-4 some clean

--- src/test/java/Task4package/Human.java
+++ src/test/java/Task4package/Human.java
@@ -1,5 +1,58 @@
 package Task4package;
 
-public class Human {
+import Task3package.Vegetable;
 
+import java.util.ArrayList;
+
+public class Human {
+private boolean genderType;
+private String name;
+private String lastName;
+private float height;
+private float weight;
+    private ArrayList<> list = new ArrayList<>();
+
+    public void add(String Name, String Color, double Weight, int Calories) {
+        Vegetable vegetable = new Vegetable(Name, Color, Weight, Calories);
+        salad.add(vegetable);
+    }
+    public boolean isGenderType() {
+        return genderType;
+    }
+
+    public void setGenderType(boolean genderType) {
+        this.genderType = genderType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
 }
